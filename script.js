bb3247a97f4134f1c3ae6bab9a01a720e17b75c4
hollybolly84540 <hollybolly84540@gmail.com>
2025-03-17 18:15:54 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function navigate(){
+    var select=document.getElementById("subtopic");
+    var selectedChoice=select.value;
+    if(selectedChoice){
+      window.location.href=selectedChoice;
+    }
+  }
+
+function sendMessage(){
+  document.addEventListener, "click", function(){
+    document.getElementById("userInput")
+  }
+}
+function aiBot(){
+  document.getElementById("chatbot-container").style.display="block";
+  }
+
+function closeChatBot(){
+  document.getElementById("chatbot-container").style.display="none";
+}
